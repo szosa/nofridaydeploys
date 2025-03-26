bc97405889ac705757177d81d1d25d8e549d28f9
Szymon Osadowski <szymon.osadowski@gmail.com>
2025-03-26 18:20:13 +0100

    Add fixtures

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Story\DefaultCompanyStory;
+use App\Story\DefaultEmployyesStory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +11,7 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        DefaultCompanyStory::load();
+        DefaultEmployyesStory::load();
     }
 }
